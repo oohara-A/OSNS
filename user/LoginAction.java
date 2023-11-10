7eb1987f9ae2f78d0fd004bc08e06389e46a29e7
柳木七音 <116529390+DwayneJohnsonJr@users.noreply.github.com>
2023-11-10 10:33:46 +0900

    Add files via upload

--- user/LoginAction.java
+++ user/LoginAction.java
@@ -6,9 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.Customer;
-import dao.CustomerDAO;
-
 public class LoginAction {
 	public String execute(
 		HttpServletRequest request, HttpServletResponse response
@@ -18,22 +15,22 @@ public class LoginAction {
 		HttpSession session=request.getSession();
 
 		// リクエストパラメータからユーザ名とパスワードとログイン日時を取得
-		String login=request.getParameter("user_name");
+		String user_name=request.getParameter("user_name");
 		String password=request.getParameter("password");
 		LocalDateTime login_date = LocalDateTime.now();
 
 		// 指定したユーザ名とパスワードの顧客をデータベースから検索する
-		CustomerDAO dao=new CustomerDAO();
-		Customer customer=dao.search(user_name, password);
+		UserDAO dao=new UserDAO();
+		User user=dao.search(user_name, password);
 
-		// ユーザ名とパスワードに合致する顧客が見つかった場合、属性名customerで登録する
-		if (customer!=null) {
-			session.setAttribute("customer", customer);
+		// ユーザ名とパスワードに合致する顧客が見つかった場合、属性名userで登録する
+		if (user!=null) {
+			session.setAttribute("user", user);
 			// login-out.jspをフォワード先に指定
-			return "login-out.jsp";
+			return "gacha.jsp";
 		}
 
 		// login-error.jspをフォワード先に指定
-		return "login-error.jsp";
+		return "sign_up.jsp";
 	}
 }
