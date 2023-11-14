df6661cf34c232054d7b44c4f5c9a98e6ec876f9
DwayneJohnsonJr <kkr2290046@stu.o-hara.ac.jp>
2023-11-14 15:07:21 +0900

    追加

--- src/admin/ShowcompanyAction.java
+++ src/admin/ShowcompanyAction.java
@@ -1,7 +1,5 @@
 package admin;
 
-import java.sql.ResultSet;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -14,28 +12,21 @@ public class ShowcompanyAction {
 		HttpSession session=request.getSession();
 		// 企業名
 		String company_name=request.getParameter("company_name");
-		// 表示数
-		int display_count=repuest.getParameter("display_count");
-		//DB接続処理
-		CompanyDAO dao=new CompanyDAO();
-		//サーチ実行処理
-		Compay company=dao.search(company_name);
 
-		ResultSet rs=st.executeQuery();
+		// 企業名がnullでないかチェック
+        if (company_name != null && !company_name.isEmpty()) {
+            // DB接続処理
+            CompanyDAO dao = new CompanyDAO();
 
-		int count = 0;
+            // サーチ実行処理
+            Company company = dao.search(company_name);
 
-		while(rs.next()){
-			//企業名
-			out.println(rs.getString("company_name"));
-			//メールアドレス
-			out.println(rs.getString("email"));
-			//登録日時
-			out.println(rs.getString(""));
-			out.println("<br>");
-			if(count==display_count){
-				break;
-			}
-		}
+            session.setAttribute("company", company);
+
+            return "company_info.jsp";
+        } else {
+            // 企業名がnullまたは空の場合
+            return "company_info.jsp";
+        }
 	}
 }
