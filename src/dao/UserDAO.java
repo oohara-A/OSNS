9ff5e8a38d986d1009eafb86f406eb58bfe91e48
LAPTOP-PH7N3V3F\adomin <kkr2290037@stu.o-hara.ac.jp>
2023-11-16 15:16:18 +0900

    11/16commit

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -4,10 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import bean.User;
 //ログイン操作など
 public class UserDAO extends DAO {
+//	ログイン時使用
 	public User search(String user_name, String password,LocalDateTime login_date)
 			throws Exception {
 			User user=null;
@@ -39,4 +41,32 @@ public class UserDAO extends DAO {
 			con.close();
 			return user;
 		}
+
+
+//	新規登録時に使用
+	public int insert(String account_name,String account_email,String account_password,LocalDateTime  adding_time)
+			throws Exception{
+		Connection con=getConnection();
+		PreparedStatement st=con.prepareStatement(
+				"insert into user(name,email,password) values(?, ?, ?)");
+//ユーザ名
+		st.setString(1, account_name);
+//		メールアドレス
+		st.setString(2, account_email);
+//		パスワード
+		st.setString(3, account_password);
+
+		 // LocalDateTimeを文字列に変換
+        String formattedTime = adding_time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        st.setString(4, formattedTime);
+
+		//SQL文実行
+		int line=st.executeUpdate();
+		st.close();
+		con.close();
+
+		return line;
+	}
+
 }
---
+++ src/user/NewregAction.java
@@ -0,0 +1,34 @@
+package user;
+
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDAO;
+import tool.Action;
+//新規登録処理
+public class NewregAction extends Action{
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response
+		) throws Exception {
+		//セッションを使えるようにする
+		HttpSession session=request.getSession();
+		//ログイン名を取得
+		String name=request.getParameter("account_name");
+//		メールアドレス
+		String email = request.getParameter("account_email");
+		//ログインパスワードを取得
+		String password=request.getParameter("account_password");
+
+//		ログイン日時
+		LocalDateTime adding_time = LocalDateTime.now();
+//新規登録
+		UserDAO dao=new UserDAO();
+		int user = dao.insert(name, email, password,adding_time);
+
+		return "sign_up.jsp";
+
+}
+}
