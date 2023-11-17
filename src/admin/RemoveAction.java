b648571381589c44e55c386e15d5e76573da0202
DwayneJohnsonJr <kkr2290046@stu.o-hara.ac.jp>
2023-11-17 13:42:09 +0900

    管理者削除変更

--- src/admin/RemoveAction.java
+++ src/admin/RemoveAction.java
@@ -18,20 +18,19 @@ public class RemoveAction {
         if (adminId != null && !adminId.isEmpty()) {
             AdminDAO adminDAO = new AdminDAO();
 
-
             if (success) {
                 return "delete_admin.jsp"; // 削除成功時に管理者一覧ページにリダイレクト
             } else {
-//                request.setAttribute("errorMessage", "管理者の削除に失敗しました。");
-//                return "error.jsp"; // エラー画面に遷移するためのJSP名を返す
+            	//request.setAttribute("errorMessage", "管理者の削除に失敗しました。");
+            	//return "error.jsp"; // エラー画面に遷移するためのJSP名を返す
 
             	// エラーアラートの表示
 
 
             }
         } else {
-//            request.setAttribute("errorMessage", "削除対象の管理者が指定されていません。");
-//            return "error.jsp"; // エラー画面に遷移するためのJSP名を返す
+        	//request.setAttribute("errorMessage", "削除対象の管理者が指定されていません。");
+        	//return "error.jsp"; // エラー画面に遷移するためのJSP名を返す
 
         	//　エラーアラートの表示
 
