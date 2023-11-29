46448c1586b421d2dcb7e1b37151d74d371d17f2
DwayneJohnsonJr <kkr2290046@stu.o-hara.ac.jp>
2023-11-29 16:35:46 +0900

    企業情報編集処理変更

--- src/company/Edit_comp_infoAction.java
+++ src/company/Edit_comp_infoAction.java
@@ -1,5 +1,58 @@
 package company;
 
-public class Edit_comp_infoAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Company;
+import dao.CompanyDAO;
+
+public class Edit_comp_infoAction {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+		// セッションを開始
+		HttpSession session=request.getSession();
+		// 氏名
+		String name=request.getParameter("name");
+		// 企業名
+		String company_name=request.getParameter("company_name");
+		// 所在地
+		String address=request.getParameter("address");
+		// メールアドレス
+		String email=request.getParameter("email");
+		// 電話番号
+		String phone_number=request.getParameter("phone_number");
+		// パスワード
+		String password=request.getParameter("password");
+		// パスワード(確認)
+		String password2=request.getParameter("password2");
+
+		// ログインされている場合
+        if (session.getAttribute("login_company")!=null) {
+        	// 入力したパスワードが一致した場合
+        	if (password==password2){
+	        	//入力した情報をデータベースに登録
+				CompanyDAO dao=new CompanyDAO();
+				Company company=dao.editcompinfo(name,company_name,address,email,phone_number,password);
+				session.setAttribute("company", company);
+				// company_top_page.jspをフォワード先に指定
+				return "company_top_page.jsp";
+			// 入力したパスワードが一致しなかった場合
+        	}else{
+        		//エラーアラートを表示
+
+        		// company_info_edit.jspをフォワード先に指定
+        		return "company_info_edit.jsp";
+        	}
+
+		//ログインされていない場合
+        }else{
+        	//エラーアラートを表示
+
+        	// company_top_page.jspをフォワード先に指定
+        	return "company_top_page.jsp";
+        }
+	}
 }
